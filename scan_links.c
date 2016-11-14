262a240a1a37d245b765422328fc35da97883926
Foo74 <foo74@protonmail.ch>
2016-11-13 17:43:45 -0800

    organized header

--- scan_links.c
+++ scan_links.c
@@ -2,30 +2,7 @@
  * Scan for broken links. Supports HTTP (port 80) and HTTPS (port 443).
  */
 
-/* Include headers */
-#include <stdio.h>         // printf() and standard i/o functions
-#include <ctype.h>         // tolower()
-#include <string.h>        // strlen(), strncat(), strstr()
-#include <sys/types.h>
-#include <sys/socket.h>    // socket(), connect()
-#include <unistd.h>        // close() to close socket, sleep() to delay
-#include <netdb.h>
-#include <arpa/inet.h>     // uint16_t htons()
-#include <netinet/in.h>    // struct sockaddr_in
-#include <stdlib.h>        // atoi()
-#include <openssl/ssl.h>   // For https comms
-
-/* Define constants */
-#define MAXBUFFER 2097152  // use a power of 2 for performance reasons
-
-/* Define functions */
-int get_html(int argc, char *argv[]);
-int remove_white_space(char from[], char to[]);
-int find_links(char buff[]);
-int check_link(char *host, char *port, char *link);
-
-const char *port80 = "80";
-const char *port443 = "443";
+#include "scan_links.h"
 
 int main(int argc, char *argv[])
 {
